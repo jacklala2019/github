d14df422dc19921c28372937e464c893053ff861
jacklala2019 <zj1803930@qq.com>
2020-10-23 17:22:34 +0800

    'son'

--- master-mana/src/App.js
+++ master-mana/src/App.js
@@ -2,7 +2,7 @@
  * @Author: Lala Jack
  * @Date: 2020-10-23 14:43:48
  * @LastEditors: Ji Lala
- * @LastEditTime: 2020-10-23 16:28:32
+ * @LastEditTime: 2020-10-23 16:31:59
  * @motto: Still water run deep
  * @Description: Modify here please
  * @FilePath: \master-mana\src\App.js
@@ -16,6 +16,12 @@ function App() {
       <header>
         hdader_son
       </header>
+      <main>
+        main_son
+      </main>
+      <footer>
+        footer-son
+      </footer>
     </div>
   );
 }
