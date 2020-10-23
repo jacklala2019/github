3f4d72b2615a7f3330856c2e711c4e884b043afd
jacklala2019 <zj1803930@qq.com>
2020-10-23 14:18:09 +0800

    '第一次'

---
+++ index.js
@@ -0,0 +1,10 @@
+/*
+ * @Author: Lala Jack
+ * @Date: 2020-10-23 14:17:31
+ * @LastEditors: Ji Lala
+ * @LastEditTime: 2020-10-23 14:17:49
+ * @motto: Still water run deep
+ * @Description: Modify here please
+ * @FilePath: \undefinedd:\桌面\main\github\index.js
+ */
+var a  = '你好';
