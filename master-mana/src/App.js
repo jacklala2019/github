cd4b0f6c566752607c49b39e75445aa722c022c7
jacklala2019 <zj1803930@qq.com>
2020-10-23 16:29:33 +0800

    son

--- master-mana/src/App.js
+++ master-mana/src/App.js
@@ -2,19 +2,20 @@
  * @Author: Lala Jack
  * @Date: 2020-10-23 14:43:48
  * @LastEditors: Ji Lala
- * @LastEditTime: 2020-10-23 14:47:40
+ * @LastEditTime: 2020-10-23 16:28:32
  * @motto: Still water run deep
  * @Description: Modify here please
  * @FilePath: \master-mana\src\App.js
  */
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      Main
+      <header>
+        hdader_son
+      </header>
     </div>
   );
 }
