40af558ff23975130cd2b4aab2cb7baed11a4d8e
jacklala2019 <zj1803930@qq.com>
2020-10-23 14:49:15 +0800

    'react'

---
+++ master-mana/src/App.js
@@ -0,0 +1,22 @@
+/*
+ * @Author: Lala Jack
+ * @Date: 2020-10-23 14:43:48
+ * @LastEditors: Ji Lala
+ * @LastEditTime: 2020-10-23 14:47:40
+ * @motto: Still water run deep
+ * @Description: Modify here please
+ * @FilePath: \master-mana\src\App.js
+ */
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      Main
+    </div>
+  );
+}
+
+export default App;
